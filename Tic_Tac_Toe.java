0f8b194996c7790e00d58c85f40d1dcf2d249ee2
Cherish <owensy.aiesec@gmail.com>
2013-07-23 12:07:12 -0700

    test2

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,5 +1,33 @@
 package Alphabeta;
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Tic_Tac_Toe {
 
+/* Complete the function below to print 2 integers separated by a single space which will be your next move */
+    static void nextMove(String player, String [] board){
+    
+  }
+  
+  public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String player;
+        String board[] = new String[3];
+
+        //If player is X, I'm the first player.
+        //If player is O, I'm the second player.
+        player = in.next();
+
+        //Read the board now. The board is a 3x3 array filled with X, O or _.
+        for(int i = 0; i < 3; i++) {
+            board[i] = in.next();
+        }
+
+        nextMove(player,board);   
+    
+    }
 }
